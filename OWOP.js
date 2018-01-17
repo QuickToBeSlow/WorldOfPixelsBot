5ecd986008f9b5dc179284843a15f67b3c730f9b
Grayson Sinclair <highwind360@gmail.com>
2018-01-17 01:49:40 -0800

    Added some neat abstractions.

--- OWOP.js
+++ OWOP.js
@@ -66,10 +66,20 @@ class PixelWand {
 	 *
 	 * @grid: an array of arrays, each full of length three arrays representing RGB
 	 */
-	drawGrid(grid) {
+	draw2DArray(grid) {
 		for (let i = 0; i < grid.length; i++)
 			for (let j = 0; j < grid.length; j++)
 				WorldOfPixels.world.setPixel(j, i, grid[i][j])
+	}
+
+	/** Renders a grid object onto the canvas
+	 *
+	 * @grid: a grid object
+	 */
+	drawGrid(grid) {
+		for (let i = 0; i < grid.width; i++)
+			for (let j = 0; j < grid.height; j++)
+				WorldOfPixels.world.setPixel(i, j, grid.getColor(i, j))
 	}
 
 	/** A function for drawing a filled rectangle
@@ -97,4 +107,76 @@ let Colors = {
 	green: [0, 255, 0],
 	blue: [0, 0, 255],
 	white: [255, 255, 255]
+}
+
+class Pixel {
+	constructor(color, above, below, left, right) {
+		this.color = color
+		this.up = above
+		this.down = below
+		this.left = left
+		this.right = right
+		this.visited = false
+	}
+
+	// returns a list of unvisited neighboring pixels
+	get unvisited() {
+		let unvisited = []
+		if (this.up && !this.up.visited)
+			unvisited.push(this.up)
+		if (this.down && !this.down.visited)
+			unvisited.push(this.up)
+		if (this.right && !this.right.visited)
+			unvisited.push(this.up)
+		if (this.left && !this.left.visited)
+			unvisited.push(this.up)
+		return unvisited
+	}
+}
+
+class Grid {
+	constructor(width, height) {
+		this.width = width
+		this.height = height
+		this.matrix = []
+
+		// Initialize the grid
+		for (let i = 0; i < this.width; i++) {
+			let newArr = []
+			for (let j = 0; j < this.height; j++)
+				newArr.push(new Pixel(Colors.black))
+			this.matrix.push(newArr)
+		}
+
+		// Lace the pixels together
+		for (let i = 0; i < this.matrix.length; i++) {
+			for (let j = 0; j < this.matrix[i].length; j++) {
+				let curNode = this.matrix[i][j]
+				if (i > 0)
+					curNode.left = this.matrix[i-1][j]
+				if (i < this.matrix.length - 1)
+					curNode.right = this.matrix[i+1][j]
+				if (j > 0)
+					curNode.up = this.matrix[i][j-1]
+				if (j < this.matrix[i].length - 1)
+					curNode.down = this.matrix[i][j+1]
+			}
+		}
+	}
+
+	// Returns the color of the pixel at coordinates x, y
+	getColor(x, y) {
+		this._validateCoords(x, y)
+		return this.matrix[x][y].color
+	}
+
+	getPixel(x, y) {
+		this._validateCoords(x, y)
+		return this.matrix[x][y]
+	}
+
+	_validateCoords(x, y) {
+		if (x < 0 || y < 0 || x >= this.width || y >= this.height)
+			throw "Cannot get the color: index out of bounds"
+	}
 }
