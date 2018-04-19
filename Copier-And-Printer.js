f91b919e5ba0a4de6427e3cc4dfd781a678de8e6
Blake Meyer <35315543+QuickToBeSlow@users.noreply.github.com>
2018-04-19 18:20:14 -0500

    Update Copier-And-Printer.js

--- Copier-And-Printer.js
+++ Copier-And-Printer.js
@@ -1,1 +1,3 @@
+Xstart = 0, Ystart = 0, XFinish = 16, YFinish = 16, Speed = 1, XPos = Xstart, YPos = Ystart, array = [];
 
+setInterval(function() {if (Mode === 1) {array.push(OWOP.getPixel(Xpos,YPos)); if (XPos >= XFinish) {XPos = 0, YPos++}}}, Speed)
